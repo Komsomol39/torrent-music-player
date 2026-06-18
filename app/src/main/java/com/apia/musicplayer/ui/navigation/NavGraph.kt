7adde741bdb3c1cd7501f6faeffc19ec06e75a23
Komsomol39 <100467630+Komsomol39@users.noreply.github.com>
2026-06-18 22:49:41 +0200

    feat(nav): navigation graph

---
+++ app/src/main/java/com/apia/musicplayer/ui/navigation/NavGraph.kt
@@ -0,0 +1,35 @@
+package com.apia.musicplayer.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.apia.musicplayer.ui.screens.library.LibraryScreen
+import com.apia.musicplayer.ui.screens.player.PlayerScreen
+import com.apia.musicplayer.ui.screens.search.SearchScreen
+import com.apia.musicplayer.ui.screens.torrent.TorrentSearchScreen
+
+sealed class Screen(val route: String) {
+    object Library : Screen("library")
+    object Player  : Screen("player")
+    object Search  : Screen("search")
+    object Torrent : Screen("torrent")
+}
+
+@Composable
+fun MusicNavGraph(navController: NavHostController) {
+    NavHost(navController = navController, startDestination = Screen.Library.route) {
+        composable(Screen.Library.route) {
+            LibraryScreen(onTrackClick = { navController.navigate(Screen.Player.route) })
+        }
+        composable(Screen.Player.route) {
+            PlayerScreen(onBack = { navController.popBackStack() })
+        }
+        composable(Screen.Search.route) {
+            SearchScreen(onTrackClick = { navController.navigate(Screen.Player.route) })
+        }
+        composable(Screen.Torrent.route) {
+            TorrentSearchScreen()
+        }
+    }
+}
